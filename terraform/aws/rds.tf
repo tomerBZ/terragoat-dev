5e2fdef06ad41ea7e8b5ec248c1dda6249b11546
Tomer David Ben Zohar <t.benzohar@gmail.com>
2024-03-14 18:38:44 +0200

    storage_encrypted      = false

--- terraform/aws/rds.tf
+++ terraform/aws/rds.tf
@@ -2,32 +2,14 @@ resource "aws_rds_cluster" "app1-rds-cluster" {
   cluster_identifier      = "app1-rds-cluster"
   allocated_storage       = 10
   backup_retention_period = 0
-  tags = {
-    git_commit           = "079fe74f6b96d887c245664fbd8cf676c92f20e5"
-    git_file             = "terraform/aws/rds.tf"
-    git_last_modified_at = "2021-12-08 23:26:32"
-    git_last_modified_by = "tron47@gmail.com"
-    git_modifiers        = "tron47"
-    git_org              = "matansha"
-    git_repo             = "terragoat"
-    yor_trace            = "b6f2c2ec-0715-46a0-83d4-502e588826d1"
-  }
+  storage_encrypted      = false
 }
 
 resource "aws_rds_cluster" "app2-rds-cluster" {
   cluster_identifier      = "app2-rds-cluster"
   allocated_storage       = 10
   backup_retention_period = 1
-  tags = {
-    git_commit           = "079fe74f6b96d887c245664fbd8cf676c92f20e5"
-    git_file             = "terraform/aws/rds.tf"
-    git_last_modified_at = "2021-12-08 23:26:32"
-    git_last_modified_by = "tron47@gmail.com"
-    git_modifiers        = "tron47"
-    git_org              = "matansha"
-    git_repo             = "terragoat"
-    yor_trace            = "d33c9292-952b-4c1f-9973-b6dbad519461"
-  }
+  storage_encrypted      = false
 }
 
 resource "aws_rds_cluster" "app3-rds-cluster" {
